ed5a08b6b18c75e65160b9e7dc2ca490e8af00fa
Matheus Rodrigues <matheusrod92@gmail.com>
2016-05-23 00:14:32 -0300

    Add Front-end

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+angular.module('NoKicks', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('green', {
+      'default': '700'
+    })
+    .accentPalette('yellow');
+});
