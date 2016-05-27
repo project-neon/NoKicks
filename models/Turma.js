ec44da936c6d808b8a8f4d6a730d73510ba963e0
Ivan Seidel <ivanseidel@gmail.com>
2016-05-27 15:49:22 -0300

    Improve Model consistency

--- models/Turma.js
+++ models/Turma.js
@@ -13,7 +13,7 @@ var PluginTimestamp = require('mongoose-timestamp');
 
 var Model = new Schema({
 
-  id: Number,
+  ufabc_id: Number,
   nome: String,
   codigo: String,
   codigoNome: String,
@@ -68,9 +68,9 @@ Model.static({
     // Find out turno
     let turno;
     if(json.nome.indexOf('Noturno') >= 0)
-      turno = 'Noturno';
+      turno = 'NOT';
     else
-      turno = 'Matutino';
+      turno = 'MAT';
 
     // Encontra o Nome da turma
     let regTurma = /[A-Z]{1}[0-9]*(?=-Matutino|-Noturno)/g;
@@ -87,8 +87,41 @@ Model.static({
     else
       campus = 'SB';
 
+    // Process horarios
+    var horarios = json.horarios.map( (horario) => {
+      let horas = horario.horas;
+      let periodo = horario.periodicidade_extenso;
+
+      let semanal = periodo.indexOf('semanal') > 0;
+      let semanaI = semanal;
+      let semanaII = semanal;
+
+      semanaI = semanaI || periodo.indexOf('(I)') > 0;
+      semanaII = semanaII || periodo.indexOf('(II)') > 0;
+
+      return {
+        inicio: horas[0],
+        final: horas[horas.length - 1],
+        dia: horario.semana,
+        semanal: semanal,
+        semanaI: semanaI,
+        semanaII: semanaII,
+      }
+    });
+
+    // Process obrigatoriedades and index by course Id
+    var obrigatoriedades = {};
+
+    json.obrigatoriedades.forEach( (obj) => {
+      var type = obj.obrigatoriedade;
+
+      type = type == 'obrigatoria' ? 'obrigatoria' : 'limitada';
+
+      obrigatoriedades[obj.curso_id] = type;
+    })
+
     let data = {
-      id: json.id,
+      ufabc_id: json.id,
       nome: json.nome,
       codigo: json.codigo,
       codigoNome: codigoNome,
@@ -100,8 +133,8 @@ Model.static({
       turno: turno,
       turma: turma,
       campus: campus,
-      horarios: json.horarios,
-      obrigatoriedade: json.obrigatoriedades,
+      horarios: horarios,
+      obrigatoriedade: obrigatoriedades,
     }
 
     return data;
