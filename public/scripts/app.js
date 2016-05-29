4a78f80101f0bebe64839ddd6ee357d916775bd7
Ivan Seidel <ivanseidel@gmail.com>
2016-05-28 22:57:52 -0300

    remove assets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,18 @@
-angular.module('NoKicks', ['ngMaterial'])
-.config(function($mdThemingProvider) {
+angular.module('NoKicks', [
+  'ngMaterial',
+	'ui.router',
+])
+
+.config( function ($mdThemingProvider) {
+
+  // Configure theme
   $mdThemingProvider.theme('default')
     .primaryPalette('green', {
       'default': '700'
     })
     .accentPalette('red');
-});
+})
+
+.controller('LoginCtrl', function (login) {
+
+})
